3b5e09c1b76a3fa13143a94d403a9c19d7fd1e74
RuneYap <runelaboratory94@gmail.com>
2022-05-09 18:07:20 +0800

    Add Notebook page and add into nav bar

--- src/App.js
+++ src/App.js
@@ -3,42 +3,69 @@ import SubtitlePlayer from "./components/SubtitlePlayer";
 import NavBar from "./components/NavBar";
 import Menu from "./components/Menu";
 import EpisodeMenu from "./components/EpisodeMenu";
+import NoteBook from "./components/NoteBook";
 import { useState } from "react";
 
 function App() {
+  const Page = {
+    MediaMenu: "MediaMenu",
+    MediaEpisode: "MediaEpisode",
+    SubtitlePlayer: "SubtitlePlayer",
+    NoteBook: "NoteBook"
+  }
   const [title, setTitle] = useState();
   const [subtitle, setSubtitle] = useState();
+  const [curPage, setCurPage] = useState(Page.MediaMenu);
 
   function onSetTitle(title) {
     setTitle(title);
     setSubtitle(null);
+    setCurPage(Page.MediaEpisode);
   }
 
   function onSetSubtitle(subtitle) {
     setSubtitle(subtitle);
   }
 
-  function onBack(){
-    if(subtitle){
+  function onBack() {
+    if (subtitle) {
       setSubtitle(null);
-    }else{
+      setCurPage(Page.MediaEpisode);
+    } else {
       setTitle(null);
+      setCurPage(Page.MediaMenu);
     }
   }
 
-  let page = <Menu key={title} title={title} onSetTitle={onSetTitle} />;
-  if(subtitle){
-    page = <SubtitlePlayer subtitle={subtitle} />;
-  }else if(title){
-    page = <EpisodeMenu partitionKey={title} onSetSubtitle={onSetSubtitle}/>
+  function onSetPage(curPage) {
+    setCurPage(curPage);
   }
+
+  let page;
+  console.log(curPage);
+  if (curPage === Page.MediaMenu) {
+    console.log("in medai menu");
+    page = <Menu key={title} title={title} onSetTitle={onSetTitle} />;
+  } else if (curPage === Page.MediaMenu) {
+    page = <EpisodeMenu partitionKey={title} onSetSubtitle={onSetSubtitle} />
+  } else if (curPage === Page.SubtitlePlayer) {
+    page = <SubtitlePlayer subtitle={subtitle} />;
+  } else if (curPage === Page.NoteBook) {
+    page = <NoteBook />;
+  }
+
+  // if (subtitle) {
+  //   page = <SubtitlePlayer subtitle={subtitle} />;
+  // } else if (title) {
+  //   page = <EpisodeMenu partitionKey={title} onSetSubtitle={onSetSubtitle} />
+  // }
 
   return (
     <div className="App bg-light">
       <header></header>
       <div className="app-body container">
         <div className="app-body row">
-          <NavBar title={title} onBack={onBack}></NavBar>
+          <NavBar title={title} onBack={onBack} onSetPage={onSetPage} curPage={curPage}></NavBar>
           {page}
         </div>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,35 @@ import "./NavBar.scss";
 
 export default function NavBar(props) {
   let backButton = null;
+  let pageBtn = null;
 
   if (props.title) {
     backButton = (
       <button
         className="btn btn-dark"
-        onClick={() => {
-          props.onBack();
-        }}
+        onClick={() => props.onBack()}
       >
         Back
       </button>
     );
+  } else {
+    backButton = (
+      <button className="btn btn-dark">
+        &nbsp;
+      </button>
+    );
+  }
+
+  if (props.curPage === "NoteBook") {
+    pageBtn = (<button className="btn btn-dark" onClick={() => props.onSetPage("SubtitlePlayer")}>Player</button>);
+  } else {
+    pageBtn = (<button className="btn btn-dark" onClick={() => props.onSetPage("NoteBook")}>NoteBook</button>);
   }
 
   return (
     <nav id="nav-bar" className="navbar navbar-expand-lg navbar-dark bg-dark">
       {backButton}
+      {pageBtn}
     </nav>
   );
 }
